51f04240e74727ead6debb32d584594da62e72da
Mayank0307 <mayank141236@gmail.com>
2021-04-07 16:31:16 +0530

    excercise_6

--- Tabs/script.js
+++ Tabs/script.js
@@ -1,35 +1,19 @@
-const tabs = document.querySelector('.tabs');
-const tabButtons = tabs.querySelectorAll('[role="tab"]');
-const tabPanels = Array.from(tabs.querySelectorAll('[role="tabpanel"]'));
+const tabs = document.querySelector(".wrapper");
+const tabButton = document.querySelectorAll(".tab-button");
+const contents = document.querySelectorAll(".content");
 
-function handleTabClick(event) {
-  // hide all tab panels
-  tabPanels.forEach(panel => {
-    panel.hidden = true;
-  });
-  // mark all tabs as unselected
-  tabButtons.forEach(tab => {
-    // tab.ariaSelected = false;
-    tab.setAttribute('aria-selected', false);
-  });
-  // mark the clicked tab as selected
-  event.currentTarget.setAttribute('aria-selected', true);
-  // find the associated tabPanel and show it!
-  const { id } = event.currentTarget;
+tabs.onclick = e => {
+  const id = e.target.dataset.id;
+  if (id) {
+    tabButton.forEach(btn => {
+      btn.classList.remove("active");
+    });
+    e.target.classList.add("active");
 
-  /*
-    METHOD 1
-  const tabPanel = tabs.querySelector(`[aria-labelledby="${id}"]`);
-  console.log(tabPanel);
-  tabPanel.hidden = false;
-  */
-
-  // METHOD 2 - find in the array of tabPanels
-  console.log(tabPanels);
-  const tabPanel = tabPanels.find(
-    panel => panel.getAttribute('aria-labelledby') === id
-  );
-  tabPanel.hidden = false;
+    contents.forEach(content => {
+      content.classList.remove("active");
+    });
+    const element = document.getElementById(id);
+    element.classList.add("active");
+  }
 }
-
-tabButtons.forEach(button => button.addEventListener('click', handleTabClick));
